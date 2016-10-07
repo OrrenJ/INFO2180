c55057163442307a636982eb10486a1e180236f9
Orren Joseph <ojey11@gmail.com>
2016-10-06 20:47:51 -0500

    Add status messages in page

--- Lab 4/maze.js
+++ Lab 4/maze.js
@@ -5,9 +5,13 @@ window.onload = function(){
 	// select all boundaries except example
 	var boundaries = document.querySelectorAll(".boundary:not(.example)");
 	var endBlock = document.getElementById("end");
+	var statusBlock = document.getElementById("status");
 
 	// start game when start block is hovered
 	startblock.onmouseover = function(){
+		// set status block
+		if(inProgress)
+			statusBlock.innerHTML = "Good luck!";
 		// set for each boudary
 		for(i=0; i < boundaries.length; i++){
 			var boundary = boundaries[i];
@@ -19,14 +23,17 @@ window.onload = function(){
 		// end clause
 		endBlock.onmouseover = function(){
 			if(inProgress){
+				// set status block
+				statusBlock.innerHTML = "You win!";
 				inProgress = false;
-				alert("You win!");
 			}
 		};
 	};
 
 	// reset game when start block is clicked
 	startblock.onclick = function(){
+		// set status block
+		statusBlock.innerHTML = "Good luck!";
 		inProgress = true;
 		for(i=0; i < boundaries.length; i++){
 			var boundary = boundaries[i];
@@ -37,6 +44,8 @@ window.onload = function(){
 
 	// function to loop through the boundaries turning them red
 	function youLose(){
+		// set status block
+		statusBlock.innerHTML = "You lose!";
 		inProgress = false;
 		for(i=0; i < boundaries.length; i++){
 			var boundary = boundaries[i];
