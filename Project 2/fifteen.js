d925f72766f7350567d3b72105ec4490bff80242
Orren Joseph <ojey11@gmail.com>
2016-10-22 17:16:16 -0500

    Reorganise code

--- Project 2/fifteen.js
+++ Project 2/fifteen.js
@@ -1,46 +1,65 @@
+// GLOBAL VARIABLES
+
+var puzzlesize = 400;	// size of the board (400px)
+var numpieces = 4;		// number of pieces on the board. 4 indicates 4x4, 5 indicates 5x, etc
+var piecesize;
+var emptysquare;		// position of the empty square
+var pieces;				// variable to store array of pieces
+
+// "main" function
 $(document).ready(function(){
 
-	var puzzlesize = 400;	// size of the board (400px)
-	var numpieces = 4;		// number of pieces on the board. 4 indicates 4x4, 5 indicates 5x, etc
-	var piecesize = puzzlesize/numpieces;
-	var emptysquare;		// position of the empty square
-	var pieces;				// variable to store array of pieces
-
 	doInitialSetup();	// perform the initial board setup
+	findValidMoves();	// find valid moves
+	randomise();		// randomise the board
 
-	// function to setup the board at the start of the game
-	function doInitialSetup(){
-
- 		emptysquare = [ numpieces-1, numpieces-1 ];
-
-		pieces = $("#puzzlearea div");
-		$(pieces).each(function(){
-			// position the pieces
-			var piecenumber = $(this).html();
-			var xpos = (piecenumber-1)%numpieces;				// 0-3
-			var ypos = Math.ceil(piecenumber/numpieces - 1);	// 0-3
-			positionPiece(this, xpos, ypos, false);
-
-			// set the background for each piece
-			$(this).addClass("puzzlepiece");
-			$(this).css({backgroundPosition: "-" + xpos*piecesize + "px -" + ypos*piecesize + "px" });	// eg, -0px -0px
-		});
-	}
-
-	// function to position a single piece
-	function positionPiece(piece, x, y, anim){
-		var xpos = x * piecesize;
-		var ypos = y * piecesize;
-		var time;
-
-		if(anim)
-			time = 250;
-		else
-			time = 0;
-
-		$(piece).animate({ left: xpos, top: ypos }, time);
-
-		$(piece).attr("x", x);
-		$(piece).attr("y", y);
-	}
 });
+
+// UTILITY FUNCTIONS
+
+// function to setup the board at the start of the game
+function doInitialSetup(){
+
+	piecesize = puzzlesize/numpieces;
+	emptysquare = [ numpieces-1, numpieces-1 ];
+
+	pieces = $("#puzzlearea div");
+	$(pieces).each(function(){
+		// position the pieces
+		var piecenumber = $(this).html();
+		var xpos = (piecenumber-1)%numpieces;				// 0-3
+		var ypos = Math.ceil(piecenumber/numpieces - 1);	// 0-3
+		positionPiece(this, xpos, ypos, false);
+
+		// set the background for each piece
+		$(this).addClass("puzzlepiece");
+		$(this).css({backgroundPosition: "-" + xpos*piecesize + "px -" + ypos*piecesize + "px" });	// eg, -0px -0px
+	});
+}
+
+// function to position a single piece
+function positionPiece(piece, x, y, anim){
+	var xpos = x * piecesize;
+	var ypos = y * piecesize;
+	var time;
+
+	if(anim)
+		time = 250;
+	else
+		time = 0;
+
+	$(piece).animate({ left: xpos, top: ypos }, time);
+
+	$(piece).attr("x", x);
+	$(piece).attr("y", y);
+}
+
+// function to find valid moves
+function findValidMoves(){
+	// TODO
+}
+
+// function to randomise the board
+function randomise(){
+	// TODO
+}
